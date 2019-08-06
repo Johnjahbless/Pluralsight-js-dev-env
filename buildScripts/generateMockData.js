212e4ca9b40a46c39b1dd02e942491f38eff3a69
June <juneapps12@gmail.com>
2019-08-06 13:59:13 +0100

    addedFetchAPI

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,22 @@
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
+import faker from 'faker';
+
+jsf.extend("faker", function(){
+return faker;
+});
+
+const outputFile = './src/api/db.json';
+//const json = JSON.stringify(jsf(schema));
+
+jsf.resolve(schema).then(function(result) {
+fs.writeFile(outputFile, JSON.stringify(result, null, 2), function(err) {
+  if(err) {
+    return console.log(chalk.red(err));
+  }else{
+console.log(chalk.green("Mock data generated"));
+  }
+});
+});
