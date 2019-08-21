ffaa9ce371036c3bb633cc1400ff7c8e461568b4
June <juneapps12@gmail.com>
2019-08-21 12:09:13 +0100

    productionBuild

---
+++ webpack.config.dev.pro.js
@@ -0,0 +1,31 @@
+import webpack from 'webpack';
+import path from 'path';
+//import config from 'webpack';
+
+export default {
+  devtool: 'source-map',
+  entry: [path.resolve(__dirname, 'src/index')],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    //Eliminate duplicate packages when generating bundle
+    //new webpack.optimize.DedupePlugin(),
+    // minify js
+    //new config.optimization.minimize,
+    new webpack.LoaderOptionsPlugin({
+      debug: false,
+      noInfo: true
+    })
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+    ]
+  }
+};
